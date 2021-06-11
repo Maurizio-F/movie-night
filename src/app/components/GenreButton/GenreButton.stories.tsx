d13cfb7eb7dbae3642a5fbb334858d7359a8c67e
Maurizio-F <end2zone@googlemail.com>
2021-06-11 11:16:19 +0200

    :lipstick: Style GenreButton

---
+++ src/app/components/GenreButton/GenreButton.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import GenreButton from "./GenreButton";
+
+export default {
+  title: "Component/GenreButton",
+  component: GenreButton,
+};
+
+export const GenreButtonComponent = (): JSX.Element => (
+  <GenreButton>Abenteuer</GenreButton>
+);
