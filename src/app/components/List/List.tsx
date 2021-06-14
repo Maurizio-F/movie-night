db56c8e45101049c469e51d3f5d0d34beaae0dc4
Maurizio-F <end2zone@googlemail.com>
2021-06-14 18:30:52 +0200

    :sparkles: Add list component

---
+++ src/app/components/List/List.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import DeleteIcon from "../Icons/DeleteIcon";
+import ShareIcon from "../Icons/ShareIcon";
+
+type ListProps = {
+  headline: string;
+  date: string;
+  shareButton?: React.SVGProps<SVGSVGElement>;
+  deleteButton: React.SVGProps<SVGSVGElement>;
+};
+
+function List({
+  headline,
+  date,
+  shareButton,
+  deleteButton,
+}: ListProps): JSX.Element {
+  return (
+    <div>
+      <p>{headline}</p>
+      <p>{date}</p>
+      <button>{shareButton}</button>
+      <button>{deleteButton}</button>
+    </div>
+  );
+}
+
+export default List;
