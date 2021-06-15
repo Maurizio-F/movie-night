f95aae6fe07515b1c05dd34f96fe456a8430d467
Maurizio-F <end2zone@googlemail.com>
2021-06-15 16:13:50 +0200

    :sparkles: Add SmallMovieCard component

---
+++ src/app/components/SmallMovieCard/SmallMovieCard.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+type CardProps = {
+  imgSrc: string;
+  movieName: string;
+  genres: string[];
+  runtime: number;
+};
+
+function SmallMovieCard({
+  imgSrc,
+  movieName,
+  genres,
+  runtime,
+}: CardProps): JSX.Element {
+  return (
+    <div>
+      <img src={imgSrc} />
+      <h2>{movieName}</h2>
+      <span>{genres}</span>
+      <span>{runtime}</span>
+    </div>
+  );
+}
