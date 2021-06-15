75c86e700280164709786a47553157c3522654da
Maurizio-F <end2zone@googlemail.com>
2021-06-15 16:32:24 +0200

    :sparkles: Add stories.tsx

---
+++ src/app/components/SmallMovieCard/SmallMovieCard.stories.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import SmallMovieCard from "./SmallMovieCard";
+
+export default {
+  title: "Component/SmallMovieCard",
+  component: SmallMovieCard,
+};
+
+export const Cruella = (): JSX.Element => {
+  return (
+    <SmallMovieCard
+      imgSrc="./cruella.jpeg"
+      movieName="Cruella"
+      genres={["Action", "Drama"]}
+      runtime={120}
+    />
+  );
+};
+
+export const MortalKombat = (): JSX.Element => {
+  return (
+    <SmallMovieCard
+      imgSrc="./mortalKombat.jpeg"
+      movieName="Mortal Kombat"
+      genres={["Action"]}
+      runtime={150}
+    />
+  );
+};
+
+export const JusticeLeague = (): JSX.Element => {
+  return (
+    <SmallMovieCard
+      imgSrc="./justiceLeague.jpeg"
+      movieName="Justice League"
+      genres={["Action", "Drama"]}
+      runtime={150}
+    />
+  );
+};
--- src/app/components/SmallMovieCard/SmallMovieCard.tsx
+++ src/app/components/SmallMovieCard/SmallMovieCard.tsx
@@ -22,3 +22,5 @@ function SmallMovieCard({
     </div>
   );
 }
+
+export default SmallMovieCard;
