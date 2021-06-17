63c1a01af4f5115f7f6bf8982379f4272f249459
Maurizio-F <end2zone@googlemail.com>
2021-06-17 11:48:39 +0200

    :sparkles: Add Homescreen page

---
+++ src/app/pages/Homescreen/Homescreen.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import HomescreenElement from "./HomescreenElement";
+
+function Homescreen(): JSX.Element {
+  return (
+    <main>
+      <img src="./logoWithText.png" />
+      <HomescreenElement />
+    </main>
+  );
+}
+
+export default Homescreen;
