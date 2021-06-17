f728ca29ad07d83122ad07a5f5ce4b64fa7021fa
Maurizio-F <end2zone@googlemail.com>
2021-06-17 17:32:37 +0200

    :lipstick: Style component

--- src/app/pages/Zufallsgenerator/zufallsgenerator.tsx
+++ src/app/pages/Zufallsgenerator/zufallsgenerator.tsx
@@ -1,25 +1,24 @@
 import React from "react";
 import NavBar from "../../components/NavBar/NavBar";
 import BigMovieCard from "../../components/BigMovieCard/BigMovieCard";
-import ShowMoreLink from "../../components/ShowMoreLink/ShowMoreLink";
 import Button from "../../components/Button/Button";
 import Footer from "../../components/Footer/Footer";
+import styles from "./Zufallsgenerator.module.css";
 
 function Zufallsgenerator(): JSX.Element {
   return (
-    <div>
+    <div className={styles.container}>
       <main>
         <div>
           <NavBar />
         </div>
         <div>
-          <BigMovieCard imgSrc={"./cruella"} movieName={"Cruella"} />
-          <ShowMoreLink link={"#"} />
+          <BigMovieCard imgSrc={"./cruella.jpeg"} movieName={"Cruella"} />
         </div>
         <div>
           <Button>Nächster Film</Button>
         </div>
-        <div>
+        <div className={styles.footer}>
           <Footer />
         </div>
       </main>
