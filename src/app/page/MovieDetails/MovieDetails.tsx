42cde3029330c163bc48c70daa1b1de9eef2744d
Maurizio-F <end2zone@googlemail.com>
2021-06-19 09:38:38 +0200

    :sparkles: Add MovieDetails Page

--- src/app/page/MovieDetails/MovieDetails.tsx
+++ src/app/page/MovieDetails/MovieDetails.tsx
@@ -1,11 +1,39 @@
 import React from "react";
+import Trailer from "../../components/Trailer/Trailer";
+import CastCard from "../../components/CastCard/CastCard";
+import AvailableStreamingSection from "../../components/AvailableStreamingSection/AvailableStreamingSection";
+import Footer from "../../components/Footer/Footer";
 
 function MovieDetails(): JSX.Element {
   return (
     <div>
-      <main></main>
+      <main>
+        <Trailer videoSrc="" />
+        <div>
+          <h2></h2>
+          <span></span>
+          <span></span>
+          <CastCard imgSrc="" name="" filmName="" />
+          <AvailableStreamingSection availableStreaming={streaming} />
+        </div>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
+
+const streaming = [
+  {
+    streamingPath: "./netflix.png",
+  },
+  {
+    streamingPath: "./amazonPrime.png",
+  },
+  {
+    streamingPath: "./disney.png",
+  },
+];
 
 export default MovieDetails;
