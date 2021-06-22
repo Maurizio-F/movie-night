90c7d1c5025bb47ae57eaaf50db7ae51a53dca44
Maurizio-F <end2zone@googlemail.com>
2021-06-22 09:02:26 +0200

    :art: Change footer svg

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -3,18 +3,16 @@ import React from "react";
 function Footer(): JSX.Element {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 428 44">
-      <g transform="translate(0 2)" fill="var(--primary-bg-color)">
-        <path
-          d="M 319.6621704101562 41.5 L 0.5 41.5 L 0.5 0.5616171956062317 L 135.6813201904297 16.3101863861084 L 319.6621704101562 41.5 Z"
-          stroke="none"
-        />
-      </g>
-      <g transform="translate(317)" fill="var(--secondary-color)">
-        <path
-          d="M 110.4999923706055 43.5 L 0.04766280576586723 43.5 L -27.70343589782715 38.16179656982422 L 110.4999923706055 0.6537871360778809 L 110.4999923706055 43.5 Z"
-          stroke="none"
-        />
-      </g>
+      <path
+        d="M0,0,135.745,15.814,327,42H0Z"
+        transform="translate(0 2)"
+        fill="var(--primary-bg-color)"
+      />
+      <path
+        d="M-36.932,44,111,0V44H-36.932Z"
+        transform="translate(317)"
+        fill="var(--secondary-color)"
+      />
     </svg>
   );
 }
