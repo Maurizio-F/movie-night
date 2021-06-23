450ac0daf20208b189fc1b2ad388d96620e0f585
Maurizio-F <end2zone@googlemail.com>
2021-06-23 14:58:58 +0200

    :sparkles: Add stories.tsx

---
+++ src/app/pages/Plan/Plan.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Plan from "./Plan";
+
+export default {
+  title: "Page/Plan",
+  component: Plan,
+  parameters: {
+    layout: "fullscreen",
+  },
+};
+
+export const Planen = (): JSX.Element => <Plan />;
