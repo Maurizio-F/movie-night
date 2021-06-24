7cbdc662b4dfc930fa9976eec0a83ac013ecd670
Maurizio-F <end2zone@googlemail.com>
2021-06-24 10:45:28 +0200

    :sparkles: Add Match page

---
+++ src/app/pages/Match/Match.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import NavBar from "../../components/NavBar/NavBar";
+import BackButton from "../../components/BackButton/BackButton";
+import BigMovieCard from "../../components/BigMovieCard/BigMovieCard";
+import Button from "../../components/Button/Button";
+import Footer from "../../components/Footer/Footer";
+
+function MatchPage(): JSX.Element {
+  return (
+    <div>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <BackButton />
+        <BigMovieCard imgSrc="./cruella.jpeg" movieName="Cruella" />
+        <Button>Teilen</Button>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+}
