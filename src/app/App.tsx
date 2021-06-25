da4cc95c165e81713656af70246704e24dd7955b
Maurizio-F <end2zone@googlemail.com>
2021-06-25 15:11:33 +0200

    :sparkles: Add routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,29 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Planen } from "./components/HomescreenLink/HomescreenLink.stories";
+import Homescreen from "./pages/Homescreen/Homescreen";
 import MovieDetails from "./pages/MovieDetails/MovieDetails";
+import { ZufallsgeneratorPage } from "./pages/Zufallsgenerator/Zufallsgenerator.stories";
 
 function App(): JSX.Element {
-  return <MovieDetails />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Homescreen />
+        </Route>
+        <Route exact path="/planen">
+          <Planen />
+        </Route>
+        <Route exact path="/zufallsgenerator">
+          <ZufallsgeneratorPage />
+        </Route>
+        <Route exact path="/movies/:id">
+          <MovieDetails />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
--- src/app/pages/Homescreen/Homescreen.tsx
+++ src/app/pages/Homescreen/Homescreen.tsx
@@ -2,16 +2,17 @@ import React from "react";
 import HomescreenLink from "../../components/HomescreenLink/HomescreenLink";
 import styles from "./Homescreen.module.css";
 import HomescreenElement from "./HomescreenElement";
+import logo from "../../assets/logoWithText.png";
 
 function Homescreen(): JSX.Element {
   return (
     <div className={styles.container}>
       <main>
-        <img className={styles.logo} src="./logoWithText.png" alt="" />
+        <img className={styles.logo} src={logo} alt="" />
       </main>
       <div className={styles.bottom}>
         <HomescreenLink text={"Planen"} link={""} />
-        <HomescreenLink text={"Zufallsgenerator"} link={""} />
+        <HomescreenLink text={"Zufallsgenerator"} link={"/zufallsgenerator"} />
         <HomescreenElement />
       </div>
     </div>
