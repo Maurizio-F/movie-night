f0c2ec16c8bfeea6b0281388dd1b0c134c546153
Maurizio-F <end2zone@googlemail.com>
2021-06-29 13:13:37 +0200

    :sparkles: Implement fallback into BigMovieCard.tsx

--- src/app/components/BigMovieCard/BigMovieCard.tsx
+++ src/app/components/BigMovieCard/BigMovieCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ShowMoreLink from "../ShowMoreLink/ShowMoreLink";
 import styles from "./BigMovieCard.module.css";
+import defaultImage from "../../assets/default.png";
 
 type CardProps = {
   imgSrc: string;
@@ -11,12 +12,21 @@ function BigMovieCard({ imgSrc, movieName }: CardProps): JSX.Element {
   return (
     <div className={styles.movieCard}>
       <div className={styles.movieCard__container}>
-        <img
+        {/* <img
           className={styles.movieCard__image}
           src={imgSrc}
           alt="Movie Poster"
-        />
+        /> */}
+        <picture>
+          <source srcSet={imgSrc} className={styles.movieCard__image} />
+          <img
+            src={defaultImage}
+            alt="Image not found"
+            className={styles.movieCard__image}
+          />
+        </picture>
       </div>
+
       <h1 className={styles.movieCard__headline}>{movieName}</h1>
       <ShowMoreLink link="/movies/337404" />
     </div>
